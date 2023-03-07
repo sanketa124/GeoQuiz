3f6e9c102197931487e37dd9ef8e94a07d730d60
sanketsfdc34@gmail.com <sanketsfdc34@gmail.com>
2023-03-07 23:17:29 +0530

    Added Snackbar

--- app/src/main/java/com/bignerdranch/androidgeoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/androidgeoquiz/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var coordinator: CoordinatorLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById<Button>(R.id.true_button)
         falseButton = findViewById<Button>(R.id.false_button)
+        coordinator = findViewById<CoordinatorLayout>(R.id.layout)
 
         trueButton.setOnClickListener{
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
         }
 
         falseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            val snackbar = Snackbar.make(coordinator, "Did Something", Snackbar.LENGTH_LONG)
+            snackbar.show()
+            //Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
 
     }
